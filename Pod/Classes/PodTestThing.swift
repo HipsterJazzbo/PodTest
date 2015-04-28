ec067ee83e3f53871b7b2248e4704db2ae6b4d94
Caleb Fidecaro <calebfidecaro@gmail.com>
2015-04-28 14:50:53 +1200

    Test file

---
+++ Pod/Classes/PodTestThing.swift
@@ -0,0 +1,16 @@
+//
+//  PodTestThing.swift
+//  Pods
+//
+//  Created by Caleb Fidecaro on 28/04/15.
+//
+//
+
+import UIKit
+
+class PodTestThing: NSObject {
+   
+    func doTheThing() -> String {
+        return "Hello"
+    }
+}
